732abac85ba2178729116c1a66345f0c6f1453b2
ametrin <itametrin@gmail.com>
2024-09-26 15:42:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3022;
+const PORT = 3021;
 
 app.use(express.json());
 app.use(express.static(__dirname));
